8bfcf1913488680f1e25b94f576dc4a4ef5dfbbd
lukaro <oh.lumito@gmail.com>
2024-05-17 00:10:32 +0600

    2.0

---
+++ App/static/js/animateText.js
@@ -0,0 +1,31 @@
+const textContainer = document.getElementById('animated-text');
+const texts = ["What if you found yourself in a place where a dream became reality?",
+               "The absurd becomes the norm",
+               "Where did everyone go?"];
+let currentIndex = 0;
+let charIndex = 0;
+let deleting = false;
+
+function type() {
+    if (!deleting && charIndex < texts[currentIndex].length) {
+        textContainer.textContent += texts[currentIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, 100);
+    } else if (deleting && charIndex > 0) {
+        textContainer.textContent = texts[currentIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(type, 50);
+    } else {
+        deleting = !deleting;
+        if (!deleting) {
+            currentIndex = (currentIndex + 1) % texts.length;
+            setTimeout(type, 3500);
+        } else {
+            setTimeout(type, 1000);
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    setTimeout(type, 3500);
+});
---
+++ README.txt
@@ -0,0 +1,10 @@
+All frontend by Bootstrap Made. I just customized it a little for myself. Backend by me.
+
+##### Frontend info #####
+
+Template Name: PhotoFolio
+Template URL: https://bootstrapmade.com/photofolio-bootstrap-photography-website-template/
+Author: BootstrapMade.com
+License: https://bootstrapmade.com/license/
+
+Camera logo on the index page by manshagraphics, https://www.flaticon.com/authors/manshagraphics
