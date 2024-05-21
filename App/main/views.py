be496f370850e3f7065de7819efc1e3e67bae7f1
lukaro <oh.lumito@gmail.com>
2024-05-21 22:06:08 +0600

    2.3

--- App/main/views.py
+++ App/main/views.py
@@ -1,11 +1,10 @@
 from . import main
 from flask import render_template
 import os
-# TODO 1 - Send emails (currently mailgun)
-# TODO 2 - os.listdir path problem
 
 
 @main.route("/")
 def index():
     return render_template("index.html",
-                           project_names=os.listdir("D:\\Python\\Problems & Projects\\Portfolio Projects Alpha\\Portfolio\\App\\static\\assets\\img\\portfolio\\thumbnails"))
+                           project_names=os.listdir(os.path.join(os.getcwd(),'App', 'static', 'assets',
+                                                                 'img', 'portfolio', 'thumbnails')))
